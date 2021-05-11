1cf69eab37728fed213603e5aedf58d0eaad3405
Tomé Jorge <tome.gaspar@gmail.com>
2021-05-11 15:41:41 +0200

    fix lint

---
+++ youTv/pages/payment_page.py
@@ -0,0 +1,21 @@
+from seleniumbase import BaseCase
+
+
+class PaymentPage:
+
+    heading = "h1[class^='styled__Title'] > span"
+    payment_iframe = "iframe[title='iframe']"
+    cc_number = "input[data-testid='creditcardNumber']"
+    cc_exp_date = "input[data-testid='creditcardCCExp']"
+    cc_cvv = "input[data-testid='creditcardCVV']"
+    cc_name = "input[data-testid='creditcardName']"
+    continue_button = "//button[@type='submit' and text()='Fortsæt']"
+
+    def fill_cc_details(self, sb, cc_number, cc_date, cc_cvv, cc_name):
+        """ fill cc details and press enter with \n on the last type """
+        sb.wait_for_text('Du er der næsten', self.heading, timeout=20)
+        sb.switch_to_frame(self.payment_iframe, 10)
+        sb.type(self.cc_number, cc_number)
+        sb.type(self.cc_exp_date, cc_date)
+        sb.type(self.cc_cvv, cc_cvv)
+        sb.type(self.cc_name, f"{cc_name}\n")
--- youTv/tests/test_user_creation.py
+++ youTv/tests/test_user_creation.py
@@ -2,15 +2,12 @@ import pytest
 import json
 from pathlib import Path
 from seleniumbase import BaseCase
-from pytest import mark
-import logging as logger
 from youtv.pages.subscription_mix_page import SubscriptionMixPage
 from youtv.pages.registration_page import RegistrationPage
 from youtv.pages.payment_page import PaymentPage
 from youtv.pages.home_page import HomePage
 from youtv.utils.write_to_csv import add_user_data_to_csv
 from youtv.utils import random_data
-
 
 home = Path(__file__).resolve().parent.resolve().parent
 user_data = home.joinpath('test_data/user_payment.json')
@@ -20,25 +17,26 @@ with open(user_data) as json_file:
 
 class TestCreateUser(BaseCase):
     def test_mix_3(self):
-        logger.info(f"Create new user with Mix 3")
         random_email = random_data.random_email_address()
         SubscriptionMixPage().visit_onboarding_page(self)
         SubscriptionMixPage().select_default_mix(self, 3)
         SubscriptionMixPage().assert_points_in_mix_wheel(self, "3")
         SubscriptionMixPage().click_continue(self)
-        RegistrationPage().fill_customer_details(self,
-            name=user_details['name'],
-            email=random_email,
-            phonenumber=user_details['phone_number'],
-            password=user_details['password'])
+        RegistrationPage(). \
+            fill_customer_details(self,
+                                  name=user_details['name'],
+                                  email=random_email,
+                                  phonenumber=user_details['phone_number'],
+                                  password=user_details['password'])
         RegistrationPage().click_accept_terms(self)
         RegistrationPage().click_accept_marketing(self)
         RegistrationPage().click_continue(self)
-        PaymentPage().fill_cc_details(self,
-            cc_number=user_details['cc_number'],
-            cc_date=user_details['exp_date'],
-            cc_cvv=user_details['cvv'],
-            cc_name=user_details['name'])
+        PaymentPage(). \
+            fill_cc_details(self,
+                            cc_number=user_details['cc_number'],
+                            cc_date=user_details['exp_date'],
+                            cc_cvv=user_details['cvv'],
+                            cc_name=user_details['name'])
         HomePage().assert_welcomed(self)
         add_user_data_to_csv(r'users.csv', mix="Mix 3", email=random_email)
 
