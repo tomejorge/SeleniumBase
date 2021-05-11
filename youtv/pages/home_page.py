d714d38b1de3bd930692d9b816843f9219f483b9
Tomé Jorge <tome.gaspar@gmail.com>
2021-05-11 16:00:55 +0200

    add youtv

---
+++ youtv/pages/home_page.py
@@ -0,0 +1,10 @@
+
+
+class HomePage:
+    # LOCATORS
+    new_user_welcome_dialog = "//div[text()='Velkommen til']"
+    header_div = "div[data-test='menu']"
+
+    def assert_welcomed(self, sb):
+        sb.assert_element(self.new_user_welcome_dialog, timeout=30)
+        sb.assert_element(self.header_div, timeout=40)
